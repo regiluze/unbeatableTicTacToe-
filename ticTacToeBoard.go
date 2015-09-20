963368975b4c9ce520174d93c41fe5268ca7e622
regiluze <regiluze@hotmail.com>
2015-09-20 19:12:24 +0200

    put nought on board

--- ticTacToeBoard.go
+++ ticTacToeBoard.go
@@ -1,11 +1,33 @@
 package main
 
 import (
-	"fmt"
+	_ "fmt"
 )
 
-func main() {
+const (
+	NOUGHT = "O"
+	CROSS  = "X"
+)
 
-	fmt.Println("kaixo")
+type BoardSnapshot [3][3]string
 
+type Position struct {
+	X int
+	Y int
+}
+
+type TicTacToeBoard struct {
+	snapshot BoardSnapshot
+}
+
+func NewTicTacToeBoard() *TicTacToeBoard {
+
+	board := &TicTacToeBoard{}
+	return board
+
+}
+
+func (board *TicTacToeBoard) PutNought(position Position) BoardSnapshot {
+	board.snapshot[position.X][position.Y] = NOUGHT
+	return board.snapshot
 }
--- ticTacToeBoard_test.go
+++ ticTacToeBoard_test.go
@@ -2,22 +2,29 @@ package main_test
 
 import (
 	. "github.com/onsi/ginkgo"
-	_ "github.com/onsi/gomega"
-	_ "github.com/regiluze/unbeatableTicTacToe-"
+	. "github.com/onsi/gomega"
+	. "github.com/regiluze/unbeatableTicTacToe-"
 )
 
 const ()
 
 var _ = Describe("Tic Tac Toe game board specs", func() {
-	var ()
+	var (
+		board              *TicTacToeBoard
+		irrelevantPosition Position
+	)
 
 	BeforeEach(func() {
+		board = NewTicTacToeBoard()
+		irrelevantPosition = Position{X: 0, Y: 0}
 	})
 
-	Describe("", func() {
-		Context("", func() {
-			It("", func() {
+	Describe("Filling the board with tokens", func() {
+		Context("when adding a nought", func() {
+			It("fills board position with a nought token", func() {
+				boardSnapshot := board.PutNought(irrelevantPosition)
 
+				Expect(boardSnapshot[irrelevantPosition.X][irrelevantPosition.Y]).To(Equal(NOUGHT))
 			})
 		})
 	})
