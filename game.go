97ab4f4975ddd90af370db319d256059995e7f33
regiluze <regiluze@hotmail.com>
2015-09-26 14:03:58 +0200

    game specs and strut added

---
+++ game.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Player interface {
+	PutToken(BoardSnapshot) Position
+}
+
+const ()
+
+type TicTacToeGame struct {
+	player1   Player
+	player2   Player
+	board     *TicTacToeBoard
+	winnerMap map[string]string
+}
+
+func NewTicTacToeGame(player1 Player, player2 Player) TicTacToeGame {
+	board := NewTicTacToeBoard()
+	winnerMap := map[string]string{
+		CROSS:  "player 1",
+		NOUGHT: "plater 2",
+		"":     "Draw",
+	}
+	game := TicTacToeGame{player1, player2, board, winnerMap}
+	return game
+
+}
+
+func (game TicTacToeGame) Start() string {
+	var board BoardSnapshot
+	var err error
+	for {
+		player1Turn := game.player1.PutToken(board)
+		board, err = game.board.PutCross(player1Turn)
+		player2Turn := game.player2.PutToken(board)
+		board, _ = game.board.PutNought(player2Turn)
+		if isOver, winner := game.board.IsOver(); isOver {
+			fmt.Println("EGI >>> winner", winner)
+			return game.winnerMap[winner]
+		}
+	}
+
+	return ""
+
+}
---
+++ game_test.go
@@ -0,0 +1,66 @@
+package main_test
+
+import (
+	_ "fmt"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/regiluze/unbeatableTicTacToe-"
+)
+
+const ()
+
+type LooserPlayer struct {
+	turn int
+}
+
+func (looser *LooserPlayer) PutToken(board BoardSnapshot) Position {
+	looser.turn += 1
+	if looser.turn == 2 {
+		return Position{0, 1}
+	}
+	if looser.turn == 3 {
+		return Position{2, 1}
+	}
+	return Position{0, 2}
+
+}
+
+type FirstLineFillerPlayer struct {
+	turn int
+}
+
+func (looser *FirstLineFillerPlayer) PutToken(board BoardSnapshot) Position {
+	looser.turn += 1
+	if looser.turn == 2 {
+		return Position{1, 0}
+	}
+	if looser.turn == 3 {
+		return Position{2, 0}
+	}
+	return Position{0, 0}
+}
+
+var _ = Describe("Tic Tac Toe game specs", func() {
+	var (
+		game         TicTacToeGame
+		winnerPlayer Player
+		looserPlayer Player
+	)
+
+	BeforeEach(func() {
+		looserPlayer = &LooserPlayer{}
+	})
+
+	Describe("When the game starts", func() {
+		Context("the player1 puts 3 croosses on first line", func() {
+			FIt("returns player1 as winner", func() {
+				winnerPlayer = &FirstLineFillerPlayer{}
+				game = NewTicTacToeGame(winnerPlayer, looserPlayer)
+				winner := game.Start()
+
+				Expect(winner).Should(Equal("player 1"))
+
+			})
+		})
+	})
+})
